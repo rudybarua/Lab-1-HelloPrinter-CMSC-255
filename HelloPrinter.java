da70ea63c630e57d69380bdd1829d7002a432d3f
rudybarua <46723864+rudybarua@users.noreply.github.com>
2019-01-15 13:31:47 -0500

    Add files via upload

---
+++ HelloPrinter.java
@@ -0,0 +1,25 @@
+/********************************************
+* HelloPrinter
+*********************************************
+* Prints out the headers and comment blocks
+*____________________________________________
+* Rudradip Barua
+* January 15, 2019
+* CMSC 255, Section 002
+********************************************/
+
+public class HelloPrinter {
+	public static void printHeading() {
+		System.out.println("Rudradip Barua");
+		System.out.println("HelloPrinter");
+		System.out.println("Print out the headers and comment blocks.");
+		System.out.println("January 15, 2019");
+		System.out.println("CMSC 255, Section 002");
+		System.out.println();
+	}
+
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
